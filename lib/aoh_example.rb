065a2d3bf2e0d3e80a4277431942b90f45c4714e
Kelsey Oshiro <kelseymosh@gmail.com>
2020-07-28 17:58:08 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -49,7 +49,8 @@ def aoh_update(aoh, row, key, new_value)
     { :name=>"Kate Gompert", :occupation=>"None" },
     { :name=>"Bruce Green", :occupation=>"Fan of Mildred" }
   ]
-  array[2][:name] = JOELLE_VD
+  new_value =
+  array[2][:name] = "Joelle van Dyne"
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
